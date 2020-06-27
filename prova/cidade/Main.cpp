1feec51bce4be469c44131368a4954cd486e5198
IgorLoredo <igorloredo12@gmail.com>
2020-06-27 19:17:40 -0300

    ficou faltando pouco

--- prova/cidade/Main.cpp
+++ prova/cidade/Main.cpp
@@ -1,25 +1,75 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 #include <string>
 
 using namespace std;
 
+
 struct no{
     char atual[3];
-   no *prox;
+    int filhos;
+    no* dir;
+    no*esq;
 };
 
+no*criarNo(char nome[]){
+    no*novo;
+    novo = new no;
+    novo->filhos = 0;
+    novo->dir = NULL;
+    novo->esq = NULL;
+    strcpy(novo->atual,nome);
+    return novo;
+}
 
+void inseri( no *root, char nome[]){
+    if(root == NULL){
+       
+        root = criarNo(nome);
+        return ;
+    }
+    if( 0 < strcmp(nome,root->atual)){
+        root->filhos +=1;
+        inseri(root->esq,nome);
+    }else{
+        if(0 > strcmp(nome,root->atual)){
+            root->filhos +=1;
+            inseri(root->dir,nome);
+        }
+    }
+  
+}
+
+
+
+void preordem(no *p){
+    
+    if (p !=NULL) {
+        preordem (p->esq);
+       preordem (p->dir);
+        cout << p->atual<< " " << p->filhos << "\n";
+       
+    }
+}
+
+void copiar(char nome[], char copia[]){
+    strncpy(copia,nome,3);
+}
 
 int main(){
-    char cidade1;
+    char cidade1[3];
     char cidade2[3],capital[3];
     cin >> capital;
-    no vetor[100];
+    no *root =criarNo(capital);
     while (cin >> cidade1 ){
-        cin >> cidade2;
-        cout << cidade1 << " " << cidade2 <<"\n";
+        cin>> cidade2;
+        char aux [3];
+        copiar(cidade1,aux);    
+        inseri(root,aux);
+        inseri(root,cidade2);
+        
     }
+    preordem(root);
     
     return 0;
 }
